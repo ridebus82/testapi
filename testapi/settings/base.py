22a1c7dc39d42518d3ece0a19b0db4d97072577b
park changyong <changyong112@naver.com>
2022-05-21 14:10:02 +0900

    asdkfjasfd

--- testapi/settings/base.py
+++ testapi/settings/base.py
@@ -99,10 +99,11 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
